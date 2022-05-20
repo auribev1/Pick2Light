cf18b2f034046229a5314e9ad20c1102a06152b2
auribev1 <auribev1@eafit.edu.co>
2022-05-20 16:56:17 -0500

    Agregar semaforo: cuando llega un mensaje en la función callback el pregunta si esa adress es de un semaforo, en caso de que si no hace nada, sino actualiza tanto estado del xbee como del semaforo (estado del semaforo es un 0 o 1 si esta en verde o rojo). Se creo la clase semaforo solo con función de actualizar.

--- Código/Xbee/com_tags.py
+++ Código/Xbee/com_tags.py
@@ -1,22 +1,31 @@
-from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, XBee64BitAddress
-from tag_class import Tag
+from digi.xbee.devices import XBeeDevice
+from tag_class import Tag, TrafficLight
 import pandas as pd
 import time
 from functions import jsongen
 
 
 def my_data_received_callback(xbee_message):
-    address = xbee_message.remote_device.get_64bit_addr()
-    data = xbee_message.data.decode("utf8")
-    tag_num = data[4:7]
-    info = data[:4]
-    conf_bit = data[7]      # Bit de confirmación
-    #print(tag_num, " ", info)
-    db.loc[(db.tag_num == tag_num) &   # Doble validación si el tag corresponde al numero y si la mac es igual al xbeeadress
-           (base + db.mac == str(address))].objects.apply(lambda x: x.state_update(info, master, conf_bit))
+    address = str(xbee_message.remote_device.get_64bit_addr())
+    if address[8:] not in traffic_lights.mac.values:
+        data = xbee_message.data.decode("utf8")
+        tag_num = data[4:7]
+        info = data[:4]
+        conf_bit = data[7]      # Bit de confirmación
+        #print(tag_num, " ", info)
+        db.loc[(db.tag_num == tag_num) &   # Doble validación si el tag corresponde al numero y si la mac es igual al xbeeadress
+               (base + db.mac == address)].objects.apply(lambda x: x.state_update(info, master, conf_bit))
+        # Actualización del semaforo
+        rack = str(db.loc[(db.tag_num == tag_num) & (base + db.mac == address)].rack.values[0])
+        info = 0 if 2 in [i.state for i in db.loc[(db.rack == rack)].objects] else 1
+        traffic_lights.loc[(traffic_lights.traffic_light == rack)].objects.apply(lambda x: x.state_update(info, master))
+
+
 
 
 db = pd.read_csv("tags_info.csv", dtype=object, sep=";")
+#db = pd.read_csv("tags_lab.csv", dtype=object, sep=";")
+traffic_lights = pd.read_csv("traffic_lights.csv", dtype=object, sep=";")
 
 master = XBeeDevice("COM13", 115200)
 master.open()
@@ -27,14 +36,18 @@ master.add_data_received_callback(my_data_received_callback)
 base = "0013A200"
 
 db["objects"] = [Tag(master, base, i, j) for i, j in zip(db.mac, db.tag_num)]
+traffic_lights["objects"] = [TrafficLight(master, base, i, j) for i, j in zip(traffic_lights.mac,
+                                                                              traffic_lights.traffic_light)]
 
-for i in range(len(db)):
-    db.objects[i].state_init(master)
+error_log = [db.objects[i].state_init(master) for i in range(len(db))]
+if any(isinstance(i, str) for i in error_log):
+    raise Exception("Revisar los siguientes tags: " + "\n".join('%s' % tag for tag in error_log if tag is not None))
+
 
 while True:
     json = jsongen(1, 10)
     db.loc[db.tag_num == json["num"]].objects.apply(lambda x: x.send_data(master, json["info"]))
-    time.sleep(2)
+    time.sleep(5)
 
 
 # Pruebas
@@ -45,9 +58,12 @@ while True:
 
 # for i in range(len(db)):
 #     print(db.objects[i].state)
+
+# for i in range(len(traffic_lights)):
+#     print(traffic_lights.objects[i].state)
 #
 # for i in range(len(db)):
-#     db.objects[i].send_data(master, "123"+str(i))
+#     db.objects[i].send_data(master, "1234")
 #
 # for i in range(len(db)):
 #     print(db.objects[i].value)
--- Código/Xbee/tag_class.py
+++ Código/Xbee/tag_class.py
@@ -22,12 +22,14 @@ class Tag:
                 if self.state == 0:
                     master.send_data(self.xbee, self.init + self.tag_num)  # manda 000 para establecer conexión
                 else:
-                    return True
+                    return None
             except:
                 count += 1
+                print("intento " + str(count))
 
             time.sleep(0.1)
-        raise Exception("Tag: " + str(self.tag_num) + " no ha respondido, verifique su estado antes de continuar")
+        return self.tag_num #Crear lista con todos los tags malos
+        #raise Exception("Tag: " + str(self.tag_num) + " no ha respondido, verifique su estado antes de continuar")
 
     def state_confirm(self, master):
         master.send_data(self.xbee, self.confirm + self.tag_num)  # manda 9999 para finalizar comunicación
@@ -39,11 +41,13 @@ class Tag:
         while count < 3:
             try:
                 if self.state == 1:
-                    master.send_data(self.xbee, str(data) + self.tag_num)  # manda 000 para establecer conexión
+                    master.send_data(self.xbee, str(data) + self.tag_num)
+                    #master.send_data_async(self.xbee, str(data) + self.tag_num)
                 else:
                     return True
             except:
                 count += 1
+                print("intento: ", count)
 
             time.sleep(0.1)
         raise Exception("Tag: " + str(self.tag_num) + " no ha respondido, verifique su estado antes de continuar")
@@ -64,9 +68,26 @@ class Tag:
                 self.post_info()
 
         elif (self.state == 1) and (conf_bit == str(1)):    # Confirmación con bit de confirmación 1
-            # raise Exception("Tag: " + self.tag_num + " probablemente tenga el botón hundido")
-            print("Tag: " + self.tag_num + " probablemente tenga el botón hundido")
-            # Se debería parar el código o printear y seguir funcionando?
+            print("Tag: " + self.tag_num + " probablemente tenga el botón hundido o el mensaje se repitio")
 
     def post_info(self):    # Función para devolver al servidor
         print("Tag: ", self.tag_num, " envía el numero: ", self.value)
+
+
+class TrafficLight:
+    green = 0
+    red = 1
+
+    def __init__(self, master, base, mac, rack):
+        self.xbee = RemoteXBeeDevice(master, XBee64BitAddress.from_hex_string(base+str(mac))) # inicializa el xbee
+        self.rack = rack
+        self.state = self.red
+
+    def state_update(self, info, master):
+        if self.state == self.red and info == self.green:
+            master.send_data(self.xbee, "000" + str(self.green) + "000")
+            self.state = self.green
+        elif self.state == self.green and info == self.red:
+            master.send_data(self.xbee, "000" + str(self.red) + "000")
+            self.state = self.red
+
